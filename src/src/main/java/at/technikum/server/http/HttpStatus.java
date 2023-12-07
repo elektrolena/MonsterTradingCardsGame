c0c59d7405589e67b96f977b1cfd7435d9326c5a
elenasteigberger <if22b060@technikum-wien.at>
2023-12-07 14:55:13 +0100

    added unit testsÂ

---
+++ src/src/main/java/at/technikum/server/http/HttpStatus.java
@@ -0,0 +1,24 @@
+package at.technikum.server.http;
+
+// THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+public enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
